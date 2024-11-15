062e3604b71e22aa187328ac792d881454e50823
Saif <saifisami2005@gmail.com>
2024-11-15 05:27:21 -0500

    PATCH /job/:id.

--- routes/jobs.js
+++ routes/jobs.js
@@ -51,4 +51,20 @@ router.get("/:id", async (req, res, next) => {
     }
 })
 
+//Updates a job, but you must be admin to use it.
+router.patch("/:id", ensureAdmin, async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, jobSchemaUpdate);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.update(req.params.id, req.body);
+        return res.json({ job });
+    } catch (err) {
+        return next(err);
+    }
+})
+
 module.exports = router;
