898488fe02c204724efdc160be0f394b430fdba7
Saif <saifisami2005@gmail.com>
2024-11-15 05:08:07 -0500

    POST /jobs.

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,3 +1,23 @@
 "use strict";
 
 const jsonschema = require("jsonschema"), express = require("express"), { BadRequestError } = require("../expressError"), { ensureAdmin } = require("../middleware/auth"), Job = require("../models/job");
+const jobSchemaNew = require("../schemas/jobNew.json"), jobSchemaUpdate = require("../schemas/jobUpdate.json"), jobSchemaSearch = require("../schemas/jobSearch.json");
+
+const router = express.Router({ mergeParams: true })
+
+router.post("/", ensureAdmin, async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, jobSchemaNew);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.create(req.body);
+        return res.status(201).json({ job });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+module.exports = router;
