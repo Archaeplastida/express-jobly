912c53481096c483cf35c3d9698ff99d6b27ba80
Saif <saifisami2005@gmail.com>
2024-11-15 04:47:49 -0500

    Added more of the necessary imports.

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,3 +1,3 @@
 "use strict";
 
-const jsonschema = require("jsonschema"), express = require("express"), { BadRequestError } = require("../expressError"), { ensureAdmin } = require("../middleware/auth");
+const jsonschema = require("jsonschema"), express = require("express"), { BadRequestError } = require("../expressError"), { ensureAdmin } = require("../middleware/auth"), Job = require("../models/job");
