98645c61fa9b6525b4f3be013b7129065449bf9c
Saif <saifisami2005@gmail.com>
2024-11-15 04:11:33 -0500

    get function testing: throws notfound if job id isn't found.

--- models/job.test.js
+++ models/job.test.js
@@ -23,7 +23,7 @@ describe("create", () => {
 describe("findAll", () => {
     test("works with no filter", async () => {
         let jobs = await Job.findAll();
-        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[1], title: "Job2", salary: 200, equity: "0.2", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[2], title: "Job3", salary: 300, equity: "0", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[3], title: "Job4", salary: null, equity: null, companyHandle: "c1", companyName: "C1", }])
+        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[1], title: "Job2", salary: 200, equity: "0.2", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[2], title: "Job3", salary: 300, equity: "0", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[3], title: "Job4", salary: null, equity: null, companyHandle: "c1", companyName: "C1", }]);
     })
 
     test("works with min salary", async () => {
@@ -33,7 +33,7 @@ describe("findAll", () => {
 
     test("works by equity", async () => {
         let jobs = await Job.findAll({ hasEquity: true });
-        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[1], title: "Job2", salary: 200, equity: "0.2", companyHandle: "c1", companyName: "C1", }])
+        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: testJobIds[1], title: "Job2", salary: 200, equity: "0.2", companyHandle: "c1", companyName: "C1", }]);
     })
 
     test("works with min salary and equity", async () => {
@@ -43,7 +43,7 @@ describe("findAll", () => {
 
     test("works by name", async () => {
         let jobs = await Job.findAll({ title: "ob1" });
-        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }])
+        expect(jobs).toEqual([{ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", companyHandle: "c1", companyName: "C1", }]);
     })
 })
 
@@ -52,6 +52,15 @@ describe("findAll", () => {
 describe("get", () => {
     test("works as intended", async () => {
         let job = await Job.get(testJobIds[0]);
-        expect(job).toEqual({ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", company: { handle: "c1", name: "C1", description: "Desc1", numEmployees: 1, logoUrl: "http://c1.img", } })
+        expect(job).toEqual({ id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1", company: { handle: "c1", name: "C1", description: "Desc1", numEmployees: 1, logoUrl: "http://c1.img", } });
+    })
+
+    test("throws not found if job id isnt' found", async () => {
+        try {
+            await Job.get(0);
+            fail();
+        } catch (err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
     })
 })
