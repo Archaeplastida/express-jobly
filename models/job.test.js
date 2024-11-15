2d96667e98c505512660faa01b2bb23ce749da50
Saif <saifisami2005@gmail.com>
2024-11-15 02:29:00 -0500

    Createe function test.

--- models/job.test.js
+++ models/job.test.js
@@ -1,8 +1,19 @@
 "use strict"
 
-const { NotFoundError, BadRequestError } = require("../expressError"), db = require("../db.js"), Job = require("./job.js"), {commonBeforeAll, commonBeforeEach, commonAfterAll, commonAfterEach, testJobIds} = require("./_testCommon.js");
+const { NotFoundError, BadRequestError } = require("../expressError"), db = require("../db.js"), Job = require("./job.js"), { commonBeforeAll, commonBeforeEach, commonAfterAll, commonAfterEach, testJobIds } = require("./_testCommon.js");
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterAll(commonAfterAll);
 afterEach(commonAfterEach);
+
+//create
+
+describe("create", () => {
+    let newJob = { companyHandle: "newJob", title: "testJob", salary: 900, equity: 0.2 }
+
+    test("works as intended", async () => {
+        let job = await Job.create(newJob);
+        expect(job).toEqual({ ...newJob, id: expect.any(Number) });
+    })
+})
