61f20ffc530de7aab91318282ca3dff44b7156bc
Saif <saifisami2005@gmail.com>
2024-11-15 07:18:22 -0500

    applyToJob.js async not found if no such job test.

--- models/user.test.js
+++ models/user.test.js
@@ -238,4 +238,13 @@ describe("applyToJob", function () {
     const res = await db.query("SELECT * FROM applications WHERE job_id= $1", [testJobIds[1]]);
     expect(res.rows).toEqual([{ job_id: testJobIds[1], username: "u1" }])
   })
+
+  test("not found if no such job", async function () {
+    try {
+      await User.applyToJob("u1", 0, "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  })
 })
