6bf2547c73d6e4a3e9ef82144db84817d24342d5
Saif <saifisami2005@gmail.com>
2024-11-15 05:58:31 -0500

    GET /jobs: works: filtering

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -35,4 +35,9 @@ describe("GET /jobs", function () {
         const resp = await request(app).get(`/jobs`);
         expect(resp.body).toEqual({ jobs: [{ id: expect.any(Number), title: "J1", salary: 1, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: expect.any(Number), title: "J2", salary: 2, equity: "0.2", companyHandle: "c1", companyName: "C1", }, { id: expect.any(Number), title: "J3", salary: 3, equity: null, companyHandle: "c1", companyName: "C1" }] });
     });
+
+    test("works: filtering", async function () {
+        const resp = await request(app).get(`/jobs`).query({ hasEquity: true });
+        expect(resp.body).toEqual({ jobs: [{ id: expect.any(Number), title: "J1", salary: 1, equity: "0.1", companyHandle: "c1", companyName: "C1", }, { id: expect.any(Number), title: "J2", salary: 2, equity: "0.2", companyHandle: "c1", companyName: "C1" }] });
+    });
 });
