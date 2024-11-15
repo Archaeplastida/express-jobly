f31641ad9ab2c25345e8cb08d19bcf4e52876b09
Saif <saifisami2005@gmail.com>
2024-11-15 00:12:12 -0500

    Created static async findAll function to find all jobs with an optional filter.

--- models/job.js
+++ models/job.js
@@ -12,4 +12,31 @@ class Job {
         let newJob = result.rows[0];
         return newJob;
     }
+
+    // Find all jobs with optional filter.
+    // searchFilter (optional): minSalary, hasEquity, title (case insensitive)
+    // returns [{ id, title, salary, equity, companyHandle, companyName }, ...]
+
+    static async findAll(searchFilters = {}){
+        const { minSalary, hasEquity, title } = searchFilters;
+        let query = `SELECT jobs.id, jobs.title, jobs.salary, jobs.equity, jobs.company_handle AS "companyHandle", companies.name AS "companyName" FROM jobs LEFT JOIN companies ON companies.handle = jobs.company_handle`, conditionals = [], queryVals = [];
+
+        if(minSalary !== undefined) {
+            queryVals.push(minSalary);
+            conditionals.push(`salary >= $${queryVals.length}`);
+        }
+
+        if(hasEquity === true) conditionals.push(`equity > 0`);
+
+        if(title !== undefined) {
+            queryVals.push(`%${title}%`);
+            conditionals.push(`title ILIKE $${queryVals.length}`);
+        }
+
+        if (conditionals.length > 0) query += " WHERE " + conditionals.join(" AND ");
+
+        query += " ORDER BY title";
+        const result = await db.query(query, queryVals);
+        return result.rows;
+    }
 }
