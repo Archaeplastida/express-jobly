cc95fe1eb352803f21d84d5f4dd0c54cb040cdcd
Saif <saifisami2005@gmail.com>
2024-11-15 00:25:46 -0500

    Created static async get function to return a company given an id.

--- models/job.js
+++ models/job.js
@@ -17,18 +17,18 @@ class Job {
     // searchFilter (optional): minSalary, hasEquity, title (case insensitive)
     // returns [{ id, title, salary, equity, companyHandle, companyName }, ...]
 
-    static async findAll(searchFilters = {}){
+    static async findAll(searchFilters = {}) {
         const { minSalary, hasEquity, title } = searchFilters;
         let query = `SELECT jobs.id, jobs.title, jobs.salary, jobs.equity, jobs.company_handle AS "companyHandle", companies.name AS "companyName" FROM jobs LEFT JOIN companies ON companies.handle = jobs.company_handle`, conditionals = [], queryVals = [];
 
-        if(minSalary !== undefined) {
+        if (minSalary !== undefined) {
             queryVals.push(minSalary);
             conditionals.push(`salary >= $${queryVals.length}`);
         }
 
-        if(hasEquity === true) conditionals.push(`equity > 0`);
+        if (hasEquity === true) conditionals.push(`equity > 0`);
 
-        if(title !== undefined) {
+        if (title !== undefined) {
             queryVals.push(`%${title}%`);
             conditionals.push(`title ILIKE $${queryVals.length}`);
         }
@@ -38,5 +38,20 @@ class Job {
         query += " ORDER BY title";
         const result = await db.query(query, queryVals);
         return result.rows;
+    }
+
+    // Returns data of a job, based on the id given.
+    // Returns { id, title, salary, equity, companyHandle, company } where the company is { handle, name, description, numEmployees, logoUrl }
+    // If an id doesn't yield a company, then it shall throw NotFoundError.
+
+    static async get(id) {
+        const jobResult = await db.query(`SELECT id, title, salary, equity, company_handle AS "companyHandle" FROM jobs WHERE id = $1`, [id]), job = jobResult.rows[0];
+        if (!job) throw new NotFoundError(`Job ID ${id} doesn't exist.`);
+
+        const companyResult = await db.query(`SELECT handle, name, description, num_employees AS "numEmployee", logo_url AS "logoUrl" FROM companies WHERE handle = $1`, [job.companyHandle]);
+        delete job.companyHandle;
+        job.company = companyResult.rows[0];
+
+        return job;
     }
 }
